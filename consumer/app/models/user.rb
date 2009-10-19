d165d632d48d85cd35a68b6f25def1c28889722a
Jakub Kuźma <qoobaa+github@gmail.com>
2009-10-19 13:00:13 +0200

    removed oauth initializer - moved credentials to model

--- consumer/app/models/user.rb
+++ consumer/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   end
 
   def self.credentials
-    OAUTH_CREDENTIALS[:cv]
+    { :key => "izXS0c1kE2BXd9Lt95sD", :secret => "VWhyk1HGE10iX8KhVhmt9wzSvHIGcS1BsMvdjswb", :options => { :site => "http://localhost:3000" } }
   end
 
   def self.consumer
--- consumer/config/initializers/oauth_consumers.rb
+++
@@ -1,9 +0,0 @@
-OAUTH_CREDENTIALS = {
-  :cv => {
-    :key => "izXS0c1kE2BXd9Lt95sD",
-    :secret => "VWhyk1HGE10iX8KhVhmt9wzSvHIGcS1BsMvdjswb",
-    :options => {
-      :site => "http://localhost:3000"
-    }
-  }
-}
